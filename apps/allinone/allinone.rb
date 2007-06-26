41f67749aaf97fbb76c3a68407dd24fda03358fe
jgre <jgre@b66a4aff-3a23-0410-9524-fd4f326b5b38>
2007-06-26 14:19:19 +0000

    Added apps that also work as daemon
    git-svn-id: https://dev.tzi.org/repo/projects/rdtn/trunk@362 b66a4aff-3a23-0410-9524-fd4f326b5b38

---
+++ apps/allinone/allinone.rb
@@ -0,0 +1,105 @@
+#!/usr/bin/env ruby
+#  Copyright (C) 2007 Janico Greifenberg <jgre@jgre.org> and 
+#  Dirk Kutscher <dku@tzi.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# $Id$
+
+$:.unshift File.join(File.dirname(__FILE__), "..", "..", "lib")
+# DTN daemon
+
+require 'optparse'
+require 'bundle'
+require 'tcpcl'
+require 'udpcl'
+require 'flutecl'
+require 'rdtnlog'
+require 'contactmgr'
+require 'storage'
+require 'clientregcl'
+require 'configuration'
+require "stats"
+require "daemon"
+
+log=RdtnLogger.instance()
+log.level=Logger::DEBUG
+
+loopInterval = 10
+duration = 1
+dest = "dtn://hamlet.dtn/test"
+terminationDelay = 0
+interactive = false
+
+opts = OptionParser.new do |opts|
+  opts.on("-d", "--dest EID", "destination EID") do |d|
+    dest=d
+  end
+  opts.on("-l", "--local EID", "local EID") do |l|
+    RdtnConfig::Settings.instance.localEid = EID.new(l)
+  end
+  opts.on("-L", "--loop INTERVAL", Integer) do |val|
+    loopInterval = val
+  end
+  opts.on("-D", "--duration SECONDS", Integer) do |val|
+    duration = val
+  end
+  opts.on("-t", "--termination-delay SECONDS", "Delay termination after the last bundle was sent.", Integer) do |val|
+    terminationDelay = val
+  end
+  opts.on("-i", "--interactive") do |val|
+    interactive = true
+  end
+end
+
+daemon = RdtnDaemon::Daemon.new(opts)
+
+if not ARGV.empty?
+  plFile = open(ARGV[0])
+else
+  plFile = $stdin
+end
+
+payload=plFile.read
+plFile.close
+
+
+b = Bundling::Bundle.new(payload)
+b.destEid = EID.new(dest)
+log.debug("sending bundle")
+EventDispatcher.instance().dispatch(:bundleParsed, b)
+
+EventLoop.after(duration) do
+  log.debug("Stopping notifier")
+  EventLoop.quit()
+end
+
+if defined?(loopInterval)
+  EventLoop.every(loopInterval.seconds) do
+    b = Bundling::Bundle.new(payload)
+    b.destEid = EID.new(dest)
+    log.debug("sending bundle")
+    EventDispatcher.instance().dispatch(:bundleParsed, b)
+  end
+end
+
+log.debug("Starting DTN daemon main loop")
+
+daemon.runLoop
+
+sleep(terminationDelay)
+ObjectSpace.each_object(Link) {|link| link.close}
+ObjectSpace.each_object(Interface) {|iface| iface.close}
+
