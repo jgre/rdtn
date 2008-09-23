35c959c6e5c18f6a222e723045285738c0c5be0f
jgre <jgre@b66a4aff-3a23-0410-9524-fd4f326b5b38>
2008-09-23 11:30:47 +0000

    Metablock ontologies can be defined by Symbols.
    git-svn-id: https://dev.tzi.org/repo/projects/rdtn/trunk@912 b66a4aff-3a23-0410-9524-fd4f326b5b38

--- lib/metablock.rb
+++ lib/metablock.rb
@@ -2,73 +2,50 @@ require "bundle"
 
 class MetadataBlock < Bundling::Block
  
- METADATA_BLOCK = 2
+  METADATA_BLOCK = 2
+  Ontologies     = {
+    :contentType => 101
+  }
 
- include GenParser
-
-  attr_accessor :metadata
-  attr_accessor :ontology
-
-  @@storePolicy = :memory
+  include GenParser
 
   field :mdblockLength, :decode => GenParser::SdnvDecoder
   field :ontology,      :decode => GenParser::SdnvDecoder
   field :metadata,      :length => :mdblockLength
 
+  def self.ontologyNumber(ontology)
+    ontology.is_a?(Symbol) ? MetadataBlock::Ontologies[ontology] : ontology
+  end
+
+  def self.ontologySymbol(ontology)
+    unless ontology.is_a?(Symbol)
+      pair = MetadataBlock::Ontologies.find {|sym, num| ontology == num}
+      pair[0] if pair
+    else
+      ontology
+    end
+  end
+
   def initialize(bundle, metadata = nil, ontology = 100)
     super(bundle)
     self.metadata = metadata
-    self.ontology = ontology
+    self.ontology = MetadataBlock.ontologyNumber(ontology)
+  end
+
+  def ontologySymbol
+    MetadataBlock.ontologySymbol(@ontology)
   end
 
   def to_s
     data = ""
     data << METADATA_BLOCK
     data << flags
-    data << Sdnv.encode(self.metadataLength)
+    data << Sdnv.encode(self.metadata.length)
     data << Sdnv.encode(self.ontology)
     data << self.metadata
     return data
   end
 
-  def metadata
-    case @@storePolicy
-    when :memory
-	@metadata
-    when :random
-	if @metadataLength: open("/dev/urandom") {|f| f.read(@metadataLength)}
-	else "" end
-    else
-	nil
-    end
-  end
-
-  def ontology
-      @ontology
-  end
-  
-  def ontology=(o)
-      @ontology = o
-  end
-  
-  def metadata=(md)
-    case @@storePolicy
-    when :memory
-	@metadata = md
-    when :random
-	@metadataLength = md.length if md
-	@metadata       = nil
-    end
-  end
-
-  def metadataLength
-    if @metadataLength: @metadataLength
-    else @metadata.length end
-  end
-
-  def MetadataBlock.storePolicy=(policy)
-    @@storePolicy = policy
-  end
 end
 
 regBundleBlock(MetadataBlock::METADATA_BLOCK, MetadataBlock)
---
+++ test/metablock_test.rb
@@ -0,0 +1,35 @@
+$: << File.join(File.dirname(__FILE__), '..', 'lib')
+
+require 'test/unit'
+require 'rubygems'
+require 'shoulda'
+require 'metablock'
+
+class TestMetadataBlock < Test::Unit::TestCase
+
+  def setup
+    @bundle = Bundling::Bundle.new('test', 'dtn://test.dtn/')
+    @meta   = MetadataBlock.new(@bundle, 'metadta', 100)
+  end
+
+  should 'serialize and parse itself' do
+    sio = StringIO.new(@meta.to_s)
+    assert_equal MetadataBlock::METADATA_BLOCK, sio.getc
+
+    meta2 = MetadataBlock.new(@bundle)
+    meta2.parse(sio)
+    assert_equal @meta.metadata, meta2.metadata
+    assert_equal @meta.ontology, meta2.ontology
+  end
+
+  should 'translate Symbols to ontology numbers' do
+    meta = MetadataBlock.new(@bundle, 'metadata', :contentType)
+    assert_equal 101, meta.ontology
+  end
+
+  should 'translate ontology number ot symbols' do
+    meta = MetadataBlock.new(@bundle, 'metadata', 101)
+    assert_equal :contentType, meta.ontologySymbol
+  end
+
+end
