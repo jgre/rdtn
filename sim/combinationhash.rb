7eb5c082a547c3b00e512c0cad108eceae1888d4
Janico Greifenberg <jgre@jgre.org>
2008-10-06 20:41:55 +0200

    Added helper method to compute all combinations of values from a hash.

---
+++ sim/combinationhash.rb
@@ -0,0 +1,21 @@
+module Sim
+
+  def Sim::hash_combinations(hash)
+    iterate = lambda do |keys|
+      key, *rest = keys
+      unless (val = hash[key]).is_a?(Enumerable)
+        hash[key] = [val]
+      end
+      if key
+        hash[key].inject([]) do |ret,val|
+          ret+iterate[rest].map {|h| h.merge({key=>val})}
+        end
+      else
+        [{}]
+      end
+    end
+
+    iterate[hash.keys]
+  end
+
+end
---
+++ test/simulator/combinationhash_test.rb
@@ -0,0 +1,47 @@
+$:.unshift File.join(File.dirname(__FILE__), '../../sim')
+
+require 'test/unit'
+require 'rubygems'
+require 'shoulda'
+require 'combinationhash'
+require 'yaml'
+
+class TestCombinationHash < Test::Unit::TestCase
+
+  def setup
+    @hash = {
+      :trace         => ['DieselNet', 'MIT', 'RWP'],
+      :receiverCount => [1, 2, 3],
+      :sendRate      => [4, 5, 6]
+    }
+    @res = Sim.hash_combinations(@hash)
+  end
+
+  should 'calculate a list of hashes with all possible value combinations' do
+    assert_equal 27, @res.length
+  end
+
+  should 'assign one element from each key to the same key in each result hash' do
+    @res.each do |h|
+      h.each {|key, val| assert @hash[key].include?(val)}
+    end
+  end
+
+  should 'assign each element from the input to the same number of outputs' do
+    elements = @hash.values.flatten.compact
+    elements.each do |el|
+      occurences = @res.find_all {|h| h.any? {|k,v| v == el}}
+      assert_equal 9, occurences.length
+    end
+  end
+
+  should 'return a list with an empty hash, when given an empty hash' do
+    assert_equal [{}], Sim.hash_combinations({})
+  end
+
+  should 'ignore entries that are not enumerable' do
+    ret = Sim.hash_combinations({:a => [1], :b => 2})
+    assert_equal [{:a => 1, :b => 2}], ret
+  end
+
+end
