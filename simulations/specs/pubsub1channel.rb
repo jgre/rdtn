4baf9807e3f72e960b8a7adcfd2ec4407b5cf561
Janico Greifenberg <jgre@jgre.org>
2008-10-16 13:30:37 +0200

    Updated PubSub simulation spec.
    * Variants for the receiver count are evaluated at runtime so that the actual
      number of nodes in the simulation can be taken into account.
    * The receivers now subscribe and unsubscribe at varying times.
    * The sender is taken from the end of the shuffled list of nodes so it does not
      coincide with the first receiver.
    * Optimizing hack for the payload.
    * Another variant for the send rate: one bundle per day.

--- simulations/specs/pubsub1channel.rb
+++ simulations/specs/pubsub1channel.rb
@@ -2,7 +2,7 @@ class PubSub1Channel < Sim::Specification
 
   def execute(sim)
     sim.trace(variants(:traces,
-             {:type => 'DieselNetParser', :tracefile => 'dieselnet_spring2007'}))
+            {:type => 'DieselNetParser', :tracefile => 'dieselnet_spring2007'}))
              #{:type => 'MITParser',       :tracefile => 'MITcontacts.txt'}))
 
     sim.nodes.linkCapacity = (11 * 10**6 / 8).to_i
@@ -10,8 +10,8 @@ class PubSub1Channel < Sim::Specification
 
     channel = 'dtn://channel1/'
 
-    receiver_count = variants(:receiver_count, 1, 10, sim.nodes.length/2, 
-                              sim.nodes.length)
+    receiver_count = variants(:receiver_count, 1, lambda {sim.nodes.length/2},
+                              lambda {sim.nodes.length})
     # Load a randomized list of node IDs
     shuffled_nodes = YAML.load_file('simulations/specs/shuffled_nodes.yml')
 
@@ -20,18 +20,30 @@ class PubSub1Channel < Sim::Specification
     receivers = shuffled_nodes.find_all do |n|
       sim.nodes.length >= n and (i+=1) <= receiver_count
     end
-    receivers.each {|n| sim.node(n).register(channel) {}}
+
+    # seed the random number generator for deterministic results
+    srand 42
+    receivers.each do |n|
+      subtime,unsubtime=variants(:subscriptionInterval, [0, nil], 
+				 [t=rand(sim.duration), t+rand(sim.duration-t)])
+      sim.at(subtime) {sim.node(n).register(channel) {}; false}
+      sim.at(unsubtime) {sim.node(n).unregister(channel); false} if unsubtime
+    end
 
     # Select one sender
-    sender = shuffled_nodes.find {|n| sim.nodes.length >= n}
+    sender = shuffled_nodes.reverse.find {|n| sim.nodes.length >= n}
 
-    data = 'a' * 1024
+    # Let the payload lie about its size so it doesn't consume memory and still 
+    # have the right simulation effect on queuing.
+    data = ''; def data.length; 1024; end
 
-    sim.at(3600 / variants(:sendRate, 1, 2, 10)) do |time|
-      sim.node(sender).sendDataTo data, channel, nil, :multicast => true
+    sim.at((3600 / variants(:sendRate, (1/24.0), 1, 2, 10)).to_i) do |time|
+      b = sim.node(sender).sendDataTo(data, channel, nil, :multicast => true,
+				      :lifetime => 86400)
       puts "#@var_idx Day #{time / (3600*24)}" if (time % (3600*24)) == 0
       time < sim.duration
     end
+
   end
 
 end
