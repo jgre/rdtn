aa963be765bcbec4a3855817afacae9d07d786a5
Janico Greifenberg <jgre@jgre.org>
2008-10-21 09:55:32 +0200

    Added expirimental variants with quota-based expiry.

--- simulations/specs/pubsub1channel.rb
+++ simulations/specs/pubsub1channel.rb
@@ -10,7 +10,7 @@ class PubSub1Channel < Sim::Specification
 
     channel = 'dtn://channel1/'
 
-    receiver_count = variants(:receiver_count, 1, lambda {sim.nodes.length/2},
+    receiver_count = variants(:receiver_count, lambda {sim.nodes.length/2},
                               lambda {sim.nodes.length})
     # Load a randomized list of node IDs
     shuffled_nodes = YAML.load_file('simulations/specs/shuffled_nodes.yml')
@@ -23,9 +23,11 @@ class PubSub1Channel < Sim::Specification
 
     # seed the random number generator for deterministic results
     srand 42
+    rnd = variants(:random_intervals, false, true)
     receivers.each do |n|
-      subtime,unsubtime=variants(:subscriptionInterval, [0, nil],
-				 [t=rand(sim.duration), t+rand(sim.duration-t)])
+      interval = rnd ? [t=rand(sim.duration), t+rand(sim.duration-t)] : [0, nil]
+      subtime,unsubtime=variants(:subscriptionInterval, interval)
+
       sim.at(subtime) {sim.node(n).register(channel) {}; false}
       sim.at(unsubtime) {sim.node(n).unregister(channel); false} if unsubtime
     end
@@ -37,9 +39,23 @@ class PubSub1Channel < Sim::Specification
     # have the right simulation effect on queuing.
     data = ''; def data.length; 1024; end
 
-    sim.at((3600 / variants(:sendRate, (1/24.0), 1, 2, 10)).to_i) do |time|
+    # Define variations for expiry: 1hour, 1day, 5days, a quota of 10 items, and
+    # a quota of 20 items.
+    lifetime = 0
+    quota    = nil
+    variants(:lifetime,
+	     lambda {lifetime = 3600;   quota = nil},
+	     lambda {lifetime = 86400;  quota = nil},
+	     lambda {lifeimte = 432000; quota = nil},
+	     lambda {lifetime = nil;    quota = 10},
+	     lambda {lifetime = nil;    quota = 20})
+
+    # Assign the quotas to the stores of all nodes for the variants with quotas
+    sim.nodes.values.each {|n| n.config.store.channelquota = quota} if quota
+
+    sim.at((3600 / variants(:sendRate, 1, 10)).to_i) do |time|
       b = sim.node(sender).sendDataTo(data, channel, nil, :multicast => true,
-				      :lifetime => 86400)
+				      :lifetime => lifetime)
       puts "#@var_idx Day #{time / (3600*24)}" if (time % (3600*24)) == 0
       time < sim.duration
     end
