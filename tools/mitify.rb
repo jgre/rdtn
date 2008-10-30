6d669fc921cad5d6b2feb5814a7492fea12a20b8
Janico Greifenberg <jgre@jgre.org>
2008-10-30 21:19:15 +0100

    Support for formats with floating point timestamps.

--- tools/mitify.rb
+++ tools/mitify.rb
@@ -5,7 +5,7 @@ def map_node(nodemap, node)
 end
 
 while line = gets do
-  if /@\d+ (\w+) <-> (\w+) (up|down)/ =~ line
-    puts line.sub(/\w+ <-> \w+/, "#{map_node(nodemap, $1)} <-> #{map_node(nodemap, $2)}")
+  if /@\d+(\.\d+)? (\w+) <-> (\w+) (up|down)/ =~ line
+    puts line.sub(/\w+ <-> \w+/, "#{map_node(nodemap, $2)} <-> #{map_node(nodemap, $3)}")
   end
 end
