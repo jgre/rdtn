bbf10677fcf058308a36a5b2c4a4304e825df2cc
Janico Greifenberg <jgre@jgre.org>
2009-01-16 11:37:44 +0100

    Return zero for empty list from all statistics functions.

--- sim/stats/statistics.rb
+++ sim/stats/statistics.rb
@@ -1,13 +1,16 @@
 class Array
   def mean
+    return 0 if empty?
     inject(0){|sum, x| sum+x} / length.to_f
   end
 
   def stdev
+    return 0 if empty?
     Math.sqrt(inject(0) {|sum, x| sum + (x-mean)**2} / (length-1))
   end
 
   def sterror
+    return 0 if empty?
     stdev / Math.sqrt(length.to_f)
   end
 end
