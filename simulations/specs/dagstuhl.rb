8e1ce39c0f2a4dca0cc78577e938346defe2c10a
Janico Greifenberg <jgre@jgre.org>
2009-01-29 15:02:01 +0100

    Preliminary experimental setup.

--- simulations/specs/dagstuhl.rb
+++ simulations/specs/dagstuhl.rb
@@ -3,113 +3,103 @@ require 'statistics'
 class Dagstuhl < Sim::Specification
 
   def execute(sim)
-    sim.trace(variants(:traces,
-            [{:type => 'MITParser', :tracefile => 'random_walk_ConnectivityDtnsim2Report'}, 'Random Walk'],
-            [{:type => 'MITParser', :tracefile => 'jgre-wdm1_ConnectivityDtnsim2Report'}, 'WDM']))
-            #{:type => 'SetdestParser', :tracefile => 'scen-s1-10000x10000-n100-m1-M19-p50-1'},
-            #[{:type => 'DieselNetParser', :tracefile => 'dieselnet_spring2007'}, 'DieselNet']))
-            #{:type => 'MITParser',       :tracefile => 'MITcontacts.txt'}))
+    sim.trace(:type => 'MITParser', :tracefile => 'jgre-wdm2_ConnectivityDtnsim2Report')
 
     sim.nodes.linkCapacity = (2 * 10**6 / 8).to_i
     sim.nodes.router variants(:router, [:epidemic, "Epidemic Routing"])
+			               #[:dpsp, "DPSP (plain)"])
 			               #[:dpspPopularity, "DPSP (popularity)"],
 				       #[:dpspShortDelay, "DPSP (short delay)"],
 				       #[:dpspHopCount, "DPSP (hop count)"],
 				       #[:dpsplifetime, "DPSP (bundle lifetime)"]
 			     #)
 
-    sim.nodes.storage_limit = 1024**2 * variants(:storage_limit,10, 30, 50, 256)
+    sim.nodes.storage_limit = 1024**2 * variants(:storage_limit,10, 30) #, 50, 256)
+    sim.nodes.subscription_range = variants(:subscription_range, 1, 5) #, 10, 100)
   
-    n_channels = 5
+    n_channels = 10
     channels = (1..n_channels).map {|i| "dtn://channel#{i}/"}
 
     receiver_count = sim.nodes.length
-    # Load a randomized list of node IDs
-    shuffled_nodes = YAML.load_file('simulations/specs/shuffled_nodes.yml')
 
-    # select the desired number of receivers
-    i = 0
-    receivers = shuffled_nodes.find_all do |n|
-      sim.nodes.length >= n and (i+=1) <= receiver_count
-    end
+    # set a fixed duration of 2 days
+    duration = 3600 * 48
 
     # seed the random number generator for deterministic results
     srand 42
-    #subs_lifetime = variants(:subscription_lifetime, 3600, 86400, 432000, nil)
+
     subs_lifetime = 86400
-    receivers.each do |n|
-      interval = [t=rand(sim.duration-subs_lifetime.to_i), subs_lifetime ? t+subs_lifetime : nil]
-      subtime, unsubtime = interval
-
-      # Randomly select two channels to subscribe to
+    (1..receiver_count).each do |n|
+      # Randomly select three channels to subscribe to
       ci1 = rand(channels.length)
-      ci2 = rand(channels.length)
-      while ci1 == ci2; ci2 = rand(channels.length); end
+      ci2 = rand(channels.length/2)
+      while ci1 == ci2; ci2 = rand(channels.length/2); end
+      ci3 = rand(channels.length/3)
+      while [ci1, ci2].include?(ci3); ci3 = rand(channels.length/3); end
 
-      #sim.at(0)   {sim.node(n).register(channel){}; false}
-      [channels[ci1], channels[ci2]].each do |channel|
+      [channels[ci1], channels[ci2], channels[ci3]].each do |channel|
+	subtime   = rand(duration-subs_lifetime.to_i)
+        unsubtime = subtime + subs_lifetime
+
 	sim.at(subtime)   {sim.node(n).register(channel){}; false}
-	sim.at(unsubtime) {sim.node(n).unregister(channel); false} if unsubtime
+	sim.at(unsubtime) {sim.node(n).unregister(channel); false}
       end
     end
 
     # Select one sender per channel
-    senders = shuffled_nodes.reverse.find_all {|n| sim.nodes.length >= n}[0, n_channels]
+    senders = (1..sim.nodes.length).to_a.shuffle[0, n_channels]
 
     # Let the payload lie about its size so it doesn't consume memory and still 
     # have the right simulation effect on queuing.
     data = ''
     # 5MB (song or short podcast), 30MB (long podcast), 50MB, 100MB (video)
-    $content_size = 1024**2 * variants(:content_size, 5, 30, 50, 100
-    #content_size = 5242880
+    $content_size = 1024**2 * variants(:content_size, 1) # 30, 50, 100)
     def data.bytesize
       $content_size
     end
 
-    # Define variations for expiry: 1hour, 6 hours, 12 hours, and a quota of
-    # 10 items
-    lifetime = 21600 # variants(:lifetime, 3600, 21600, 43200, nil)
-			       #[lifetime = 3600,   quota = nil])
-			       #[lifetime = 86400,  quota = nil],
-			       #[lifeimte = 432000, quota = nil],
-			       #[lifetime = nil,    quota = 10],
-			       #[lifetime = nil,    quota = 15]
+    # Bundles expire after 6 hours
+    lifetime = 21600
 
-    # Assign the quotas to the stores of all nodes for the variants with quotas
-    sim.nodes.values.each{|n| n.config.store.channelquota = 15} if lifetime == 0
-
-    sim.at(3600) do |time|
-      senders.each_with_index do |sender, i|
-	b = sim.node(sender).sendDataTo(data, channels[i], nil,:multicast=>true,
-					:lifetime => lifetime)
+    senders.each_with_index do |sender, i|
+      sim.at(3600 + rand(1800)) do |time|
+	sim.node(sender).sendDataTo(data, channels[i], nil,:multicast=>true,
+				    :lifetime => lifetime)
+	time < duration
       end
-      puts "#@var_idx Day #{time / (3600*24)}" if (time % (3600*24)) == 0
-      time < (3600 * 48) #sim.duration
     end
 
+    sim.at(3600*24) do |time|
+      puts "#@var_idx Day #{time / (3600*24)}"
+      time < duration
+    end
   end
 
   def analyze(analysis)
     analysis.gnuplot = true
 
-    analysis.configure_data :combine => :router, :x_axis => :storage_limit do |row, x, network, traffic|
-      row.value "delivered", traffic.numberOfDeliveredBundles
-      row.value "expected (greedy)",  traffic.numberOfExpectedBundles
-      #row.value "expected (opportunistic)",  traffic.numberOfExpectedBundles(:net => network)
-      delays = traffic.delays
+    analysis.configure_data do |row, network, traffic|
+      row.value "# delivered bundles", traffic.numberOfDeliveredBundles
+      delays = traffic.delays.map {|delay| delay / 3600.0}
       row.value "delay", delays.mean
       row.std_error "delay", delays.sterror
+      row.value "# contacts", network.numberOfContacts
+      row.value "# bundles", traffic.numberOfExpectedBundles
+      row.value "contact duration", network.averageContactDuration
+      row.value "successful transmissions (MB)",traffic.bytesTransmitted/1024**2
+      bufferUse = traffic.bufferUse(3600).map {|use| (use / 1024**2) / row.value(:storage_limit).to_f * 100}
+      row.value "buffer use", bufferUse.mean
+      row.std_error "buffer use", bufferUse.sterror
+      row.value "failed transmissions (MB)", traffic.failedTransmissions/1024**2
     end
 
-    analysis.plot :combine => :router, :y_axis => ["delivered", "expected (greedy)"], :only_once => ["expected (greedy)"], :x_axis => :storage_limit #do |plot|
-      #plot.ylabel "# Bundles"
-      #plot.xlabel "Bundle Lifetime"
-    #end
+    [:storage_limit, :subscription_range].each do |x_axis|
+      analysis.plot :combine => :router, :y_axis => ["# delivered bundles"], :x_axis => x_axis
+      analysis.plot :combine => :router, :y_axis => ["delay"], :x_axis => x_axis
+      analysis.plot :combine => :router, :y_axis => ["successful transmissions (MB)", "failed transmissions (MB)"], :x_axis => x_axis
+      analysis.plot :combine => :router, :y_axis => ["buffer use"], :x_axis => x_axis
+    end
 
-    analysis.plot :combine => :router, :y_axis => ["delay"], :x_axis => :storage_limit #do |plot|
-    #  plot.ylabel "delay [seconds]"
-    #  plot.xlabel "Bundle Lifetime"
-    #end
   end 
 
 end
