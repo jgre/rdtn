d1c209d4d9cfb0498f00523fb50468295c09fbc0
Janico Greifenberg <jgre@jgre.org>
2009-01-30 14:49:16 +0100

    Test for the correct method to be called to set the subscription range.

--- sim/core.rb
+++ sim/core.rb
@@ -60,7 +60,7 @@ module Sim
 	each_value {|node| node.config.store.limit = limit}
       end
       def @nodes.subscription_range=(range)
-	each_value {|node| node.router.subsRange = range if node.router.respond_to? :subsRange}
+	each_value {|node| node.router.subsRange = range if node.router.respond_to? :subsRange=}
       end
 
       RdtnTime.scheduleFunc = lambda {|sec, &handler| after(sec, &handler)}
