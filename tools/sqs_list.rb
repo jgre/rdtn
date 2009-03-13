36a44d68b64e0ddb4392d25dbe043890ec6f3993
Janico Greifenberg <jgre@jgre.org>
2009-03-13 11:42:32 +0100

    Delete entries from the queue.

--- tools/sqs_list.rb
+++ tools/sqs_list.rb
@@ -17,13 +17,22 @@ q        = AWS::SQS::Queue.new(QUEUE, client)
 msg_count = q.get_queue_attributes("ApproximateNumberOfMessages")
 puts "Approximate Number of Messages: #{msg_count}"
 
-msg_count.to_i.times do
-  q.receive_messages.each do |msg|
-    puts msg.inspect
-    message_id = msg["Message"][0]["MessageId"][0]
-    body       = CGI.unescape(msg["Message"][0]["Body"][0])
-    puts "Message #{message_id}"
-    puts body
-    puts
-  end
+loop do
+  msg  = q.receive_messages(1)
+  break if msg.empty? || msg[0].empty?
+  body = CGI.unescape(msg[0]["Message"][0]["Body"][0])
+  receipt_handle = msg[0]["Message"][0]["ReceiptHandle"][0]
+  q.delete_message receipt_handle
+  puts body.inspect
 end
+
+# msg_count.to_i.times do
+#   q.receive_messages.each do |msg|
+#     puts msg.inspect
+#     message_id = msg["Message"][0]["MessageId"][0]
+#     body       = CGI.unescape(msg["Message"][0]["Body"][0])
+#     puts "Message #{message_id}"
+#     puts body
+#     puts
+#   end
+# end
